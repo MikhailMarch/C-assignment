805af6992f2152686befa8c2a1241baa220da3d5
Mike <morser1243@gmail.com>
2021-01-26 21:19:51 +0300

    commit

---
+++ Project/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(106, 145);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(60, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Tag = "day";
+            this.radioButton1.Text = "день";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(106, 193);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(69, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Tag = "month";
+            this.radioButton2.Text = "месяц";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.RadioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(106, 241);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(50, 21);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Tag = "year";
+            this.radioButton3.Text = "год";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.RadioButton3_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(106, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(270, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(106, 311);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(270, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(294, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите количество дней / месяцев / годов";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 279);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Какой день будет?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Project/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            TextBox objTextBox = (TextBox)sender;
+            string value = objTextBox.Text;
+            if (value.Equals("")) {
+                textBox2.Text = "";
+                return;
+            }
+
+            if (!int.TryParse(value, out _))
+            {
+                MessageBox.Show("ошибка ввода, введите число", "Предупрежение",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox2.Text = "";
+                return ;
+            }
+            DateTime thisDay = DateTime.Now;
+            DateTime output;
+            if (radioButton1.Checked) {
+                output = thisDay.AddDays(Int32.Parse(value));
+            } else if (radioButton2.Checked) {
+                output = thisDay.AddMonths(Int32.Parse(value));
+            } else if (radioButton3.Checked) {
+                output = thisDay.AddYears(Int32.Parse(value));
+            } else {
+                MessageBox.Show("Выберите на сколько хотите увеличить", "Предупрежение",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            textBox2.Text = output.ToString("dddd");
+    }
+
+        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            string value = textBox1.Text;
+            if (value.Equals("")){
+                textBox2.Text = "";
+                return;
+            }
+
+            if (!int.TryParse(value, out _))
+            {
+                MessageBox.Show("ошибка ввода, введите число", "Предупрежение",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox2.Text = "";
+                return;
+            }
+            DateTime thisDay = DateTime.Now;
+            DateTime output;
+            output = thisDay.AddDays(Int32.Parse(value));
+            textBox2.Text = output.ToString("dddd");
+        }
+
+        private void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            string value = textBox1.Text;
+            if (value.Equals(""))
+            {
+                textBox2.Text = "";
+                return;
+            }
+
+            if (!int.TryParse(value, out _))
+            {
+                MessageBox.Show("ошибка ввода, введите число", "Предупрежение",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox2.Text = "";
+                return;
+            }
+            DateTime thisDay = DateTime.Now;
+            DateTime output;
+            output = thisDay.AddMonths(Int32.Parse(value));
+            textBox2.Text = output.ToString("dddd");
+        }
+
+        private void RadioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            string value = textBox1.Text;
+            if (value.Equals(""))
+            {
+                textBox2.Text = "";
+                return;
+            }
+
+            if (!int.TryParse(value, out _))
+            {
+                MessageBox.Show("ошибка ввода, введите число", "Предупреждение",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox2.Text = "";
+                return;
+            }
+            DateTime thisDay = DateTime.Now;
+            DateTime output;
+            output = thisDay.AddYears(Int32.Parse(value));
+            textBox2.Text = output.ToString("dddd");
+        }
+    }
+}
